fb2f02fee330cac237bbfbbbfa9b503d082c0e10
realzvqle <realzvqle@gmail.com>
2024-09-16 12:38:01 -0500

    license update

--- kernel/src/hal.cpp
+++ kernel/src/hal.cpp
@@ -3,13 +3,6 @@
 #include "text.hpp"
 #include <cstdint>
 
-
-
-
-
-
-
-
 static ramfb rmfb;
 static text text;
 
@@ -21,20 +14,13 @@ void HAL::halt_system(){
     }
 }
 
-void HAL::kernel_suicide(char* error){
-    
+void HAL::kernel_suicide(char* error){  
     uint8_t white[3] = {255, 255, 255};
-    uint8_t purple[3] = {255, 0, 255};
-
-    
+    uint8_t purple[3] = {255, 0, 255};   
     rmfb.clear_background(purple);
     text.draw_text(700, 600, ":(", 10, white);
     text.draw_text(10, 10, "zivOS has ran into an issue and has been shutdown to prevent damage", 1, white);
     text.draw_text(10, 20, "please contact zvqle with this error code", 1, white);
     text.draw_text(10, 60, error, 1, white);
-
-
     halt_system();
-
-
 }
